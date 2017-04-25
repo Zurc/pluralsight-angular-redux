4e30c653bc466d55e13b83c730e1a06d9244e4d8
Zurc <cruzjurado@gmail.com>
2017-04-25 16:07:01 +0100

    make store and state available to your CourseListComponent, bind courses

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CourseService } from './course.service';
 import { Course } from './course';
 import { FilterTextComponent, FilterService } from '../blocks/filter-text';
+import { store } from "../store/store";
 
 @Component({
   selector: 'app-course-list',
@@ -20,15 +21,25 @@ export class CourseListComponent implements OnInit {
     this.filteredCourses = this._filterService.filter(searchText, ['id', 'name', 'topic'], this.courses);
   }
 
-  getCourses() {
-    this._courseService.getCourses()
-      .subscribe(courses => {
-        this.courses = this.filteredCourses = courses;
-      });
+  // getCourses() {
+  //   this._courseService.getCourses()
+  //     .subscribe(courses => {
+  //       this.courses = this.filteredCourses = courses;
+  //     });
+  // }
+
+  updateFromState() {
+    const allState = store.getState();  // get state from store
+    this.courses = allState.courses;  // assign (bind) state courses to courses variable
+    this.filteredCourses = allState.courses;  //  bind state filtered courses from state
   }
 
   ngOnInit() {
-    this.getCourses();
+    this.updateFromState();    // bind the component to initial state
+    //  bind the component to updated state when store changes
+    store.subscribe(() => {    
+      this.updateFromState();
+    })
     componentHandler.upgradeDom();
   }
 }
