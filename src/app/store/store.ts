61404dcedfb52a4b413f59529bc7b37123b7a44c
Zurc <cruzjurado@gmail.com>
2017-04-27 14:43:27 +0100

    removing unused lines on store

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,15 +1,7 @@
-import { createStore, applyMiddleware, compose, GenericStoreEnhancer } from 'redux';
+import { createStore } from 'redux';
 import { reducer } from './reducer';
-import { IAppState } from './IappState';
 
 declare var window: any; 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const DevToolsExtension: GenericStoreEnhancer = (window.DevToolsExtension) 
-    ? window.DevToolsExtension() : (f) => f;
-
-// export const store = createStore<IAppState>(reducer, 
-//     compose(DevToolsExtension) as GenericStoreEnhancer);
 
 export const store = createStore(
    reducer, /* preloadedState, */
